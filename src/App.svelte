aadcfeea0a511c2f3258da3a729d8fb566b209f1
Andrei Alisenok <andreialisenok@mdao.team>
2024-05-17 17:19:09 +0300

    bug fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,94 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+// import Menu from './components/Menu.svelte';
+// import Search from './components/Search.svelte';
+
+// import data from './data.json';
+
+const data = [{
+        "title": "Справочники",
+        "children": {
+            "title": "Справочники1",
+            "children": {
+                "title": "Справочники2"
+            }
+        }
+    },
+    {
+        "title": "Контрагенты"
+    },
+    {
+        "title": "Категории товаров"
+    },
+    {
+        "title": "Товары",
+        "children": {
+            "title": "Товары1"
+        }
+    }
+]
+let filteredItems = [];
+
+// const handleInput = () => {
+// return filteredItems = data.filter(item => item.toLowerCase().match(inputValue.toLowerCase()));
+// }
+// subName: [{
+
+//         name: 'Контрагенты'
+//     }, {
+//         name: 'Категории товаров'
+//     }, {
+//         name: 'Товары'
+//     }, {
+//         name: 'Регионы'
+//     }, {
+//         name: 'Брак'
+//     }, {
+//         name: 'Зоны'
+//     }, {
+//         name: 'Тары'
+//     }, {
+//         name: 'Тележки'
+//     }, {
+//         name: 'Типы упаковки'
+//     }, {
+//         name: 'Причина замены ячейки'
+//     }, {
+//         name: 'Причины отмены процесса перемещения '
+//     }, {
+//         name: 'Типы исключительной ситуаций'
+//     }, {
+//         name: 'Типы брака'
+//     },
+
+// ]
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div class="wrap">
 
-  <div class="card">
-    <Counter />
-  </div>
+    <ul>
+        {#each data as item}
+        <li>
+            <button> {item.title}</button>
+            {#if item.children}
+            <span>{console.log(item.children)}</span>
+            <ul>
+                {#each data as item}
+                <li>
+                    <button>{item.title}</button>
+                </li>
+                {/each}
+            </ul>
+            {/if}
+        </li>
+        {/each}
+    </ul>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+.wrap {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
 </style>
